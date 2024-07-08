c2b000be9584285f084fc1a693e227f042a0723d
Albert Allagulov <goonnors@gmail.com>
2024-07-08 11:09:43 +0300

    random elements visibiliy :)

--- src/components/TextTags.vue
+++ src/components/TextTags.vue
@@ -1,4 +1,74 @@
 <script>
+function throttle(mainFunction, delay = 300) {
+  let timerFlag = null; // Variable to keep track of the timer
+
+  // Returning a throttled version
+  return function (...args) {
+    if (timerFlag === null) {
+      // If there is no timer currently running
+      mainFunction.call(this, ...args); // Execute the main function
+      timerFlag = setTimeout(() => {
+        // Set a timer to clear the timerFlag after the specified delay
+        timerFlag = null; // Clear the timerFlag to allow the main function to be executed again
+      }, delay);
+    }
+  };
+}
+function debounce(func, timeout = 300) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+const handleResize = function () {
+  this.$nextTick(() => {
+    const globalWidth =
+      this.$refs.textTags.getBoundingClientRect().width -
+      Number(
+        window.getComputedStyle(this.$refs.textTags).paddingLeft.slice(0, 2)
+      ) -
+      Number(
+        window.getComputedStyle(this.$refs.textTags).paddingRight.slice(0, 2)
+      );
+
+    const { y: globalY, bottom: globalBottom } =
+      this.$refs.textTags.getBoundingClientRect();
+    const globalHeightBoundary = globalBottom;
+
+    let row = [];
+    let tmpRows = [];
+    let sumWidth = 0;
+
+    this.tags.forEach((tag, index) => {
+      const item = this.$refs.textTagsItem[index];
+      const width = item.getBoundingClientRect().width;
+      const { y, bottom } = item.getBoundingClientRect();
+      const heightBoundary = bottom + 20;
+      // const visible = heightBoundary < globalHeightBoundary;
+      const visible = true;
+      console.log(index, heightBoundary < globalHeightBoundary);
+      const excessWidth =
+        !visible && row[row.length - 1] && row[row.length - 1].visible;
+
+      sumWidth += width + 25;
+
+      if (sumWidth >= globalWidth || excessWidth) {
+        sumWidth = 0;
+        tmpRows.push(row);
+        row = [];
+      }
+
+      row.push({ index, visible, ...tag });
+    });
+
+    tmpRows.push(row);
+    this.rows = tmpRows;
+  });
+};
+
 export default {
   props: {
     tags: {
@@ -27,53 +97,7 @@ export default {
     window.removeEventListener("resize", this.onResize);
   },
   methods: {
-    onResize: function () {
-      this.$nextTick(() => {
-        const globalWidth =
-          this.$refs.textTags.getBoundingClientRect().width -
-          Number(
-            window.getComputedStyle(this.$refs.textTags).paddingLeft.slice(0, 2)
-          ) -
-          Number(
-            window
-              .getComputedStyle(this.$refs.textTags)
-              .paddingRight.slice(0, 2)
-          );
-
-        const { y: globalY, bottom: globalBottom } =
-          this.$refs.textTags.getBoundingClientRect();
-        const globalHeightBoundary = globalY + globalBottom;
-
-        let row = [];
-        let tmpRows = [];
-        let sumWidth = 0;
-
-        this.tags.forEach((tag, index) => {
-          const item = this.$refs.textTagsItem[index];
-          const width = item.getBoundingClientRect().width + 25;
-          const { y, bottom } = item.getBoundingClientRect();
-          const heightBoundary = y + bottom;
-          const visible = heightBoundary < globalHeightBoundary;
-          const excessWidth =
-            !visible && row[row.length - 1] && row[row.length - 1].visible;
-
-          sumWidth += width;
-
-          if (sumWidth >= globalWidth || excessWidth) {
-            sumWidth = 0;
-            tmpRows.push(row);
-            row = [];
-          }
-
-          row.push({ index, visible, ...tag });
-        });
-
-        if (row.length) {
-          tmpRows.push(row);
-        }
-        this.rows = tmpRows;
-      });
-    },
+    onResize: handleResize,
   },
 };
 </script>
@@ -86,9 +110,9 @@ export default {
         textTags__row_alignLeft: myAlign === 'left',
         textTags__row_alignJustify: myAlign === 'justify',
       }"
-      v-for="row in rows"
+      v-for="(row, rowIndex) in rows"
     >
-      <template v-for="rowItem in row">
+      <template v-for="(rowItem, index) in row">
         <div
           ref="textTagsItem"
           class="textTags__item"
@@ -98,11 +122,10 @@ export default {
             {{ rowItem.icon }}
           </v-icon>
           <span>
-            {{ rowItem.id }}:
             {{ rowItem.text }}
           </span>
         </div>
-        <v-icon v-if="rowItem.visible && rowItem.index !== row.length - 1">
+        <v-icon v-if="rowItem.visible && index !== row.length - 1">
           mdi-circle-small
         </v-icon>
       </template>
@@ -119,6 +142,7 @@ export default {
   height: 8rem;
   padding: 2.5rem;
   margin: 2.5rem 0;
+  width: 100%;
 }
 .textTags__row {
   display: inline-block;
@@ -132,6 +156,7 @@ export default {
 }
 .textTags__item {
   display: inline-block;
+  text-align: center;
 }
 .textTags__item_invisible {
   opacity: 0;
